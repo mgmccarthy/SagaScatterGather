65b259805c1c3412db8dd239fbb2fe8d83478bf4
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-10-14 22:19:29 -0400

    All vendor quote handlers implemented

---
+++ SagaScatterGather.Vendor1.Endpoint/QuoteRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using SagaScatterGather.Shared.Messages;
+
+namespace SagaScatterGather.Vendor1.Endpoint
+{
+    public class QuoteRequestHandler : IHandleMessages<Vendor1QuoteRequest>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<QuoteRequestHandler>();
+
+        public Task Handle(Vendor1QuoteRequest message, IMessageHandlerContext context)
+        {
+            Log.Info("Handling Vendor1QuoteRequest");
+            return context.Reply(new Vendor1QuoteResponse { QuoteId = message.QuoteId, QuoteAmount = 200 });
+        }
+    }
+}
---
+++ SagaScatterGather.Vendor3.Endpoint/QuoteRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using SagaScatterGather.Shared.Messages;
+
+namespace SagaScatterGather.Vendor3.Endpoint
+{
+    public class QuoteRequestHandler : IHandleMessages<Vendor3QuoteRequest>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<QuoteRequestHandler>();
+
+        public Task Handle(Vendor3QuoteRequest message, IMessageHandlerContext context)
+        {
+            Log.Info("Handling Vendor3QuoteRequest");
+            return context.Reply(new Vendor3QuoteResponse { QuoteId = message.QuoteId, QuoteAmount = 300 });
+        }
+    }
+}
